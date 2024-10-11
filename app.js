354af8b359ea7fb58cd32c0ac6ebd7f6def39364
ross-sbriscia-lark <139995797+ross-sbriscia-lark@users.noreply.github.com>
2024-10-11 13:35:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 const http = require('http');
-var url = require("url");
-var querystring = require("querystring");
 const responseBody = 
 `
-<h1 style="color: green">Hello Larker!</h1>
-<h2 style="color: green">Environment variable 'MY_VAR' value is ${process.env.MY_VAR}</h2>
+<h1 style="color: blue">Hello Larker!</h1>
+<h2 style="color: blue">Environment variable MY_VAR is ${process.env.MY_VAR || "[not set]"}</h2>
 `
-
 http.createServer().on('request', (request, response) => {
-    let body = [];
-
+    let body = []
     request.on('data', (chunk) => {
-        body.push(chunk);
+        body.push(chunk)
     }).on('end', () => {
-
-        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.writeHead(200, { 'Content-Type': 'text/html' })
         response.write(responseBody)
-        response.end();
-    });
+        response.end()
+        console.log(`${new Date().toISOString()} - ${request.socket.remoteAddress} ${request.method} ${request.url} ${response.statusCode}`)
+    })
 }).listen(8080, '0.0.0.0', () => {
     console.log(`Server is running on port 8080...`)
-});
+})
